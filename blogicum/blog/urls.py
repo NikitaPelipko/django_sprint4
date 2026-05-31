cf4a7318355d3faa25577ed336baae16a80d59fc
plsrflttr <nikitapelipko@gmail.com>
2026-05-31 13:40:37 +0300

    fixed pk naming in urls

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,9 +5,9 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("posts/<int:id>/", views.PostDetailView.as_view(), name="post_detail"),
-    path("posts/<int:pk>/edit/", views.EditPostView.as_view(), name="edit_post"),
-    path("posts/<int:pk>/delete/", views.DeletePostView.as_view(), name="delete_post"),
+    path("posts/<int:post_id>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("posts/<int:post_id>/edit/", views.EditPostView.as_view(), name="edit_post"),
+    path("posts/<int:post_id>/delete/", views.DeletePostView.as_view(), name="delete_post"),
     path("posts/create/", views.CreatePostView.as_view(), name="create_post"),
     path("posts/<int:post_id>/comment/", views.CommentCreateView.as_view(), name="add_comment"),
     path("posts/<int:post_id>/edit_comment/<int:comment_id>/", views.CommentUpdateView.as_view(), name="edit_comment"),
